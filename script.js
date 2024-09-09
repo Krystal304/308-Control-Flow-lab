b3da0df66c9bb9e0ab26fde3dfd5cf74b3b45881
Krystal304 <krystalwilliams864@gmail.com>
2024-09-09 16:31:50 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// const PI = 3.1415
+let pi = 3.1415;
+
+// const radius = 5 meters
+let radius = 5;
+
+// minimum space
+let space = 0.8;
+
+// const area = PI * radius * radious
+let area = 3.1415 * 5 * 5;
