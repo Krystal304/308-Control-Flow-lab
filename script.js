f219ccd4ee496e9757b3cad60cf887c22e2573a3
Krystal304 <krystalwilliams864@gmail.com>
2024-09-09 20:02:57 -0400

    part 2

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+
+// Part 1
 // const PI = 3.1415
 let pi = 3.1415;
 
@@ -18,13 +20,13 @@ let plant = 20;
 let start = 20 * 0.8; 
 
 // week 1 
-let week1 = 20 * 2 * 0.8
+let week1 = 20 * 2 * 0.8;
 
 // week 2
-let week2 = 20 * 4 * 0.8
+let week2 = 20 * 4 * 0.8;
 
 // week 3
-let week3 = 20 * 8 * 0.8
+let week3 = 20 * 8 * 0.8;
 
 
 // week 1
@@ -48,7 +50,7 @@ if (week1 > area * 0.8) {
         } else {
             console.log ('planted');
         }
-        
+
 // week 3
         if (week3 > area * 0.8) {
             console.log ( 'pruned');
@@ -59,3 +61,22 @@ if (week1 > area * 0.8) {
             } else {
                 console.log ('planted');
             }
+
+// Part 2
+// The conservation area in which the garden is located has multiple other gardens. 
+// Using the logic you have already created, determine:
+// The amount of additional space that would be required if the scientists 
+// were to start with 100 plants, and did not prune them for 10 weeks.
+// If the space remained circular, what would be the radius of this expanded garden?
+
+let garden = 100;
+
+// week10 = plants * space * 2 ** 10
+
+let week10 = 100 * 0.8 * 2 ** 10;
+
+let exp = week10 / area;
+console.log (exp)
+
+// Part 3
+
